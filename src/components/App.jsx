00b8f1e717d4ae44aae1f5b6110519924b357026
Mike <spormuv@gmail.com>
2023-05-25 14:52:18 +0300

    feat: async query

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import Header from './header/Header';
 import RecipeItem from './recipe-item/RecipeItem';
+import User from './user/User';
 
 function App() {
   return (
     <section>
       <Header />
+      <User />
       <div>
         <RecipeItem
           recipe={{
--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { reducer as favoriteReducer } from './favorites/favorites.slice';
+import { userSlice } from './user/user.slice';
 
-const reducers = combineReducers({ favorites: favoriteReducer });
+const reducers = combineReducers({
+  favorites: favoriteReducer,
+  user: userSlice.reducer,
+});
 
 export const store = configureStore({
   reducer: reducers,
