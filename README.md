e621a9956d8d16264f0a0cee7fab137f7d917282
Mike <spormuv@gmail.com>
2023-06-08 19:23:10 +0300

    fix: small bugs

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 ## General overview of the Redux Toolkit + RTK Query
+
+## To start:
+
+```
+1. npm i
+2. npx json-server --watch db.json
+3. npm run dev
+```
--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,15 +1,15 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 import { IRecipe } from '../../types/recipe.types';
 
-const API_URL = 'http://localhost:4200/recipes';
+const API_URL = 'http://localhost:3000/recipes';
 
 export const api = createApi({
   reducerPath: 'api',
   tagTypes: ['Recipe'],
   baseQuery: fetchBaseQuery({ baseUrl: API_URL }),
-  endpoints: (builder) => ({
+  endpoints: builder => ({
     getRecipes: builder.query<IRecipe[], string>({
-      query: (searchTerm) => `/?_sort=id&_order=desc&q=${searchTerm}`,
+      query: searchTerm => `/?_sort=id&_order=desc&q=${searchTerm}`,
       providesTags: (result, error, searchTerm) => [
         {
           type: 'Recipe',
